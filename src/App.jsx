776677d9cfded802e7ceec4e5217d3fbd7a4e791
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-17 20:11:13 -0300

    create vite React project with tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <section className="h-screen w-full bg-amber-300">
+        <h1>Hello world</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
