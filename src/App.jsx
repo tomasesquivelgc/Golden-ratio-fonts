e2138ba1a3afd516bcceec051a7acbc5a5403996
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-18 21:54:45 -0300

    add golden ratio width

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ function App() {
 
   return (
     <>
-      <section className="h-screen w-full bg-amber-300">
-        <h1>Hello world</h1>
+      <section className="h-screen w-full bg-amber-300 flex">
+        <div className="h-full w-gr bg-red-800"></div>
       </section>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'gr': '61.8%',
+      }
+    },
   },
   plugins: [],
 }
