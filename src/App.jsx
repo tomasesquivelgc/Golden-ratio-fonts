c10229a2b47fc61158269263abe060bcd6019fee
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-22 12:29:26 -0300

    installed ESlint

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 function App() {
-
   return (
     <>
       <section className=" h-screen w-full">
 
-        <nav className="w-full h-gr5 bg-gradient"></nav>
+        <nav className="w-full h-gr5 bg-gradient" />
 
-        <div className="w-full h-gr3 bg-oxfordBlue"></div>
+        <div className="w-full h-gr3 bg-oxfordBlue" />
 
         <div className="w-full h-gr1 flex">
           <div className="w-gr2 h-full">
-            <div className="w-full h-gr2 bg-gradient"></div>
-            <div className="w-full h-gr1 bg-mikadoYellow"></div>
+            <div className="w-full h-gr2 bg-gradient" />
+            <div className="w-full h-gr1 bg-mikadoYellow" />
           </div>
-          <div className="w-gr1 h-full bg-white"></div>
+          <div className="w-gr1 h-full bg-white" />
         </div>
 
-        <div className="w-full h-gr6 bg-richBlack"></div>
-      
+        <div className="w-full h-gr6 bg-richBlack" />
+
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
