1ea8354b66fc9ed94a88ab48fa12164712189fd8
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-26 15:01:24 -0300

    remove unused overflow class

--- src/components/Calculator/NumberList.jsx
+++ src/components/Calculator/NumberList.jsx
@@ -4,7 +4,7 @@ function NumberList({ calculatedNumbers }) {
   return (
     <div className="w-full h-gr1 items-center flex flex-col">
       <h2 className="text-gr3 md:text-gr2">Calculated</h2>
-      <ul className=' overflow-y-scroll w-gr2 text-center'>
+      <ul className=' w-gr2 text-center'>
         {calculatedNumbers.map((number) => (
           <li className='text-gr4 md:text-gr3' key={number}>{number}</li>
         ))}
