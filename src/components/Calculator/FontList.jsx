5e83531a7eb7f8889222b021ddc532cf431dbfef
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-27 14:20:16 -0300

    fix linters

--- src/components/Calculator/FontList.jsx
+++ src/components/Calculator/FontList.jsx
@@ -6,11 +6,17 @@ function FontList({ biggestNumber, calculatedNumbers, units }) {
       <h2 className="text-gr3 md:text-gr2 border-b p-0 m-1 max-w-fit self-center">Font List</h2>
       <ul>
         <li style={{ fontSize: biggestNumber + units }}>
-          Font Size {biggestNumber}{units}
+          Font Size
+          {' '}
+          {biggestNumber}
+          {units}
         </li>
         {calculatedNumbers.map((number) => (
           <li key={number} style={{ fontSize: number + units }}>
-            Font Size {number}{units}
+            Font Size
+            {' '}
+            {number}
+            {units}
           </li>
         ))}
       </ul>
