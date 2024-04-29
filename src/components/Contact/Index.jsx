34ab8ccee2829cfe95dbc40e5ec59d409cb56663
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-04-29 16:57:19 -0300

    add text to about section

--- src/components/Contact/Index.jsx
+++ src/components/Contact/Index.jsx
@@ -1,13 +1,13 @@
-function Contact(){
-  return(
+function Contact() {
+  return (
     <div id="contact" className="hidden w-full h-gr1 justify-center items-center text-white">
       <div className="flex w-gr2 h-full flex-col">
-        <div className="h-gr2 w-full border-b"></div>
-        <div className="h-gr1 w-full"></div>
+        <div className="h-gr2 w-full border-b" />
+        <div className="h-gr1 w-full" />
       </div>
-      <div className="w-gr1 border-l h-full"></div>
+      <div className="w-gr1 border-l h-full" />
     </div>
-  )
+  );
 }
 
 export default Contact;
